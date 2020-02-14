5631e20d7808f9ed0f816170a4db2cc0b81a7ead
Prof Edir <ecaramba@gmail.com>
2020-02-13 21:51:16 -0300

    aula sobre node

--- ajax/listagem.php
+++ ajax/listagem.php
@@ -1,5 +1,19 @@
 <?php
 
+    $host = "banco.db";
+    $user = "";
+    $pass = "";
+
+    $db = new PDO("sqlite:banco.db");
+
+    $sql = "select * from contatos order by nome";
+
+    $retorno = $db->query($sql);
+
+    $valores = $retorno->fetchAll(PDO::FETCH_ASSOC);
+
+   // var_dump($valores);
+    
     $contatos[0]["nome"] = "Joselito";
     $contatos[0]["email"] = "joselito@abc.com";
     $contatos[0]["telefone"] = "(12)1234-1234";
@@ -11,8 +25,9 @@
     $contatos[1]["telefone"] = "(21)2121-12121";
     $contatos[1]["cidade"] = "Colombo";
 
+   // var_dump($contatos);
 
-   echo json_encode($contatos);
+   echo json_encode($valores);
     
 
 ?>
